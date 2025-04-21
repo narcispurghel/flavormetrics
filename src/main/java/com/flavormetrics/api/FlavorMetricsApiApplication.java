a6f0c66171531e3b5551f719218e1c6ced237ccb
Narcis Purghel <narcispurghel@gmail.com>
2025-04-22 01:55:21 +0300

    Initial commit

---
+++ src/main/java/com/flavormetrics/api/FlavorMetricsApiApplication.java
@@ -0,0 +1,13 @@
+package com.flavormetrics.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavorMetricsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavorMetricsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flavormetrics/api/FlavorMetricsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavormetrics.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavorMetricsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
