dadbed672a92ab8157e93dbd25d67d3a4f6610d2
Narcis Purghel <narcispurghel@gmail.com>
2025-04-22 23:09:04 +0300

    Add Spring Security

---
+++ src/main/java/com/flavormetrics/api/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.flavormetrics.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers("/api/**").permitAll();
+                    request.requestMatchers("/api/test/auth").hasAnyRole("ADMIN", "USER", "NUTRITIONS");
+                    request.anyRequest().authenticated();
+                })
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
