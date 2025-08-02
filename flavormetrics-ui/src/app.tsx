9d2cfcf3e91e4fbad0eb97d67099790ec0e7463c
Narcis Purghel <narcispurghel@gmail.com>
2025-08-03 01:16:47 +0300

    feat: Add navigation

--- flavormetrics-ui/src/app.tsx
+++ flavormetrics-ui/src/app.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom"; // Importă Routes și Route
+import { Navigation } from "./components/navigation";
 import { LoginView } from "./login-view";
 import { ListaRetete } from "./recipe";
 import { RecipeView } from "./recipe-view";
@@ -6,13 +7,16 @@ import { SignupView } from "./signup-view";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<ListaRetete />} />
-        <Route path="/recipe/:id" element={<RecipeView />} />
-        <Route path="/login" element={<LoginView />} />
-        <Route path="/signup" element={<SignupView />} />
-      </Routes>
+    <div className="w-screen flex flex-col h-fit gap-20">
+      <Navigation />
+      <div>
+        <Routes>
+          <Route path="/" element={<ListaRetete />} />
+          <Route path="/recipe/:id" element={<RecipeView />} />
+          <Route path="/login" element={<LoginView />} />
+          <Route path="/signup" element={<SignupView />} />
+        </Routes>
+      </div>
     </div>
   );
 }
